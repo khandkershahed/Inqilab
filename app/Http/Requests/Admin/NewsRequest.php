92a65bf35bc4566b1927175687a103206d518aa7
shahed141123 <khandkershahed23@gmail.com>
2025-06-02 15:30:55 +0600

    22-05-25

--- app/Http/Requests/Admin/NewsRequest.php
+++ app/Http/Requests/Admin/NewsRequest.php
@@ -29,6 +29,8 @@ class NewsRequest extends FormRequest
             'video_url'             => 'nullable|url',
             'thumbnail'             => 'nullable|image|mimes:jpg,jpeg,png,webp|max:3072',
             'banner_image'          => 'nullable|image|mimes:jpg,jpeg,png,webp|max:4096',
+            'thumbnail'             => 'nullable|url',
+            'banner_image'          => 'nullable|url',
 
             'category_id'           => 'nullable|exists:categories,id',
             'sub_category_id'       => 'nullable|exists:categories,id',
