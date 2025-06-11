2f024ad9d1220800355339f218cb5640225a466b
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2025-06-11 19:39:24 +0600

    Slug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Exception;
+use App\Models\Setting;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Set default values
+        View::share('setting', null);
+
+        try {
+            // Check for table existence and set actual values
+            if (Schema::hasTable('settings')) {
+                View::share('setting', Setting::first());
+            }
+
+        } catch (Exception $e) {
+            //
+        }
+
+        Paginator::useBootstrap();
     }
 }
